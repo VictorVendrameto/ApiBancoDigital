7d3fdbfa74d138843f8b4ffeeb951a7e4fea7189
VictorVendrameto <vendrametovictor@gmail.com>
2023-04-13 15:56:53 -0300

    commit 1

--- rotas.php
+++ rotas.php
@@ -1,1 +1,14 @@
 <?php
+
+use ApiBancoDigital\Controller\EnderecoController;
+
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+//rotas pro banco digital
+switch ($url)
+{
+    default:
+        http_response_code(403);
+    break;
+}
