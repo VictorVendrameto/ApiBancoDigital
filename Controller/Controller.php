706f515d9be1f07a61fe77346c900d5854134ca6
VictorVendrameto <vendrametovictor@gmail.com>
2023-04-17 09:34:18 -0300

    commit 2

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,1 +1,57 @@
 <?php
+
+namespace ApiBancoDigital\Controller;
+
+use Exception;
+
+abstract class Controller
+{
+    public static function getResponseAsJSON($data)
+    {
+        header("Acess-Control-Allow-Origin: *");
+        header("Content-type: application/json; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: public");
+
+        exit(json_encode($data));
+    }
+
+    protected static function getExceptionAsJSON(Exception $e)
+    {
+        $exception = [
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'traceAsString' => $e->getTraceAsString(),
+            'previous' => $e->getPrevious()
+        ];
+
+        http_response_code(400);
+
+        header("Acess-Control-Allow-Origin: *");
+        header("Content-type: application/json; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: public");
+
+        exit(json_encode($exception));
+    }
+
+    protected static function isGet()
+    {
+        if($_SERVER['REQUEST_METHOD'] !=='GET')
+            throw new Exception("O método da requisição deve ser GET");
+    }
+
+    protected static function isGet()
+    {
+        
+    }
+
+    protected static function isGet()
+    {
+        
+    }
+}
