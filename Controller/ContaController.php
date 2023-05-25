48cb6439b86663b86006e980ed63148c6acb3958
VictorVendrameto <vendrametovictor@gmail.com>
2023-05-25 15:50:52 -0300

    commit 6

--- Controller/ContaController.php
+++ Controller/ContaController.php
@@ -1,16 +1,43 @@
 <?php
 namespace ApiBancoDigital\Controller;
 
+use ApiBancoDigital\Model\ContaModel;
+use Exception;
+
 class ContaController extends Controller
 {
-    public static function save()
+    public static function save() : void
     {
+        try
+        {
+            $json_obj = json_decode(file_get_contents('php://input'));
 
+            $model = new ContaModel();
+            $model->id = $json_obj->Id;
+            $model->tipo = $json_obj->Tipo;
+
+            $model->save();
+        }
+        catch (Exception $e)
+        {
+            parent::getExceptionAsJSON($e);
+        }
     }
 
     public static function select()
     {
-        
+        try
+        {
+            $model = new ContaModel();
+
+            $model->getAllRows();
+
+            parent::getResponseAsJSON($model->rows);
+        }
+        catch (Exception $e)
+        {
+            parent::getExceptionAsJSON($e);
+        }
     }
 
     public static function extrato()
@@ -30,6 +57,17 @@ class ContaController extends Controller
 
     public static function delete()
     {
-        
+        try
+        {
+            $model = new ContaModel();
+
+            $model->id = parent::getIntFromUrl(isset($_GET['id']) ? $_GET['id'] : null);
+
+            $model->delete();
+        }
+        catch (Exception $e)
+        {
+            parent::getExceptionAsJSON($e);
+        }
     }
 }
