338c8a2dc3b80a3e7a771a6e6c453ab78d4879e5
VictorVendrameto <vendrametovictor@gmail.com>
2023-06-19 08:33:19 -0300

    Adição do banco

---
+++ Banco/BancoDigital.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE banco_digital;
+
+USE banco_digital;
+
+CREATE TABLE correntista
+(
+	id INT AUTO_INCREMENT,
+    nome VARCHAR(150),
+    email VARCHAR(100),
+    data_nasc DATE,
+    cpf CHAR(11),
+    senha VARCHAR(100),
+    data_cadastro DATE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE conta
+(
+	id INT AUTO_INCREMENT,
+    numero VARCHAR(150),
+    tipo ENUM("Corrente", "Poupança"),
+    saldo DOUBLE, 
+    limite DOUBLE,
+    senha VARCHAR(100),
+    data_abertura DATE,
+    id_correntista INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_correntista) REFERENCES correntista(id)
+);
+
+CREATE TABLE chave_pix
+(
+	id INT AUTO_INCREMENT,
+    tipo ENUM("CPF", "Número Celular", "E-mail", "Aleatório"),
+    chave VARCHAR(150),
+    id_conta INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_conta) REFERENCES conta (id)
+); 
+
+CREATE TABLE transacao
+(
+	id INT AUTO_INCREMENT,
+    valor DOUBLE,
+    data_transacao TIMESTAMP,
+    id_conta_remetente INT,
+    id_conta_destinatario INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_conta_remetente) REFERENCES conta (id),
+    FOREIGN KEY (id_conta_destinatario) REFERENCES conta (id)
+);
+
+CREATE TABLE conta_transacao_assoc
+(
+	id INT AUTO_INCREMENT,
+    id_conta INT,
+    id_transacao INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_conta) REFERENCES conta (id),
+    FOREIGN KEY (id_transacao) REFERENCES transacao (id)
+);
+
+INSERT INTO correntista (nome, email, data_nasc, cpf, senha, data_cadastro) VALUES ("hugo", "hugo@gmail.com", "2005-09-20", "12345678910", sha1("123"), "2023-11-22");
+
+INSERT INTO conta (numero, saldo, limite, tipo, senha, data_abertura, id_correntista) VALUES ("987654321", "1250.98", "5000", "Corrente", "7355608", "2023-05-15", 1);
