ac73b795c4028cf62828775897a8c8f79c79396a
VictorVendrameto <vendrametovictor@gmail.com>
2023-06-20 21:18:04 -0300

    arrumando erros do banco

--- Controller/CorrentistaController.php
+++ Controller/CorrentistaController.php
@@ -6,31 +6,76 @@ use ApiBancoDigital\Model\CorrentistaModel;
 
 class CorrentistaController extends Controller
 {
-    public static function save()
+    public static function save() : void
     {
-        $json_obj = parent::getJSONFromRequest();
+        try
+        {  
+            $json_obj = json_decode(file_get_contents('php://input'));
 
-        $model = new CorrentistaModel();
-        $model->id = $json_obj->id;
-        $model->cpf = $json_obj->cpf;
-        $model->senha = $json_obj->senha;
-        $model->data_nasc = $json_obj->data_nasc;
+            $model = new CorrentistaModel();
+
+            $model->id = $json_obj->id;
+            $model->nome = $json_obj->nome;
+            $model->cpf = $json_obj->cpf;
+            $model->data_nasc = $json_obj->data_nasc;
+            $model->email = $json_obj->email;
+            $model->senha = $json_obj->senha;
+            $model->data_cad = $json_obj->data_cad;
+        
 
         $model->save();
+        }
+        catch(Exception $e)
+        {
+            parent::getExceptionAsJSON($e);
+        }
     }
 
-    public static function select()
+    public static function select() : void
     {
-        
+        try
+        {
+            $model = new CorrentistaModel();
+            
+            $model->getAllRows();
+
+            parent::getResponseAsJSON($model->rows);
+              
+        } catch (Exception $e) {
+
+            parent::getExceptionAsJSON($e);
+        }
     }
 
     public static function auth()
     {
-        
+        try 
+        {
+            $json_obj = json_decode(file_get_contents('php://input'));
+
+            $model = new CorrentistaModel();
+            $model->cpf = $json_obj->Cpf;
+            $model->senha = $json_obj->Senha;
+
+            parent::getResponseAsJSON($model->AuthenticCorrentista());
+        } catch (Exception $err) {
+            parent::getExceptionAsJSON($err);
+        }
     }
 
-    public static function delete()
+    public static function delete() : void
     {
-        
+        try
+		{
+			$model = new CorrentistaModel();
+
+			$model->id = parent::getIntFromUrl(isset($_GET['id']) ? $_GET['id'] : null);
+
+			$model->delete();
+		}
+		catch(Exception $e)
+		{
+			parent::getExceptionAsJSON($e);
+		}
     }
 }
--- DAO/CorrentistaDAO.php
+++ DAO/CorrentistaDAO.php
@@ -11,28 +11,56 @@ class CorrentistaDAO extends DAO
         parent::__construct();
     }
 
-    public function select()
+    public function select() : array
     {
+        $sql = "SELECT * FROM correntista";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(DAO::FETCH_CLASS, "ApiBancoDigital\Model\CorrentistaModel");
     }
 
-    public function selectById()
+    public function selectByCpfSenha(CorrentistaModel $model)
     {
+        $sql = "SELECT * FROM correntista WHERE cpf = ? AND senha = ?";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $model->cpf);
+        $stmt->bindValue(2, $model->senha);
+        $stmt->execute();
+
+        return $stmt->fetchObject("ApiBancoDigital\Model\CorrentistaModel");
     }
 
     public function update()
     {
 
     }
-
-    public function insert()
+    public function insert(CorrentistaModel $m) : bool
     {
+        $sql = "INSERT INTO correntista (nome, cpf, data_nasc, email, senha, 
+            data_cad) VALUES (?, ?, ?, ?, sha1(?), ?)";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $m->nome);
+        $stmt->bindValue(2, $m->cpf);
+        $stmt->bindValue(3, $m->data_nasc);
+        $stmt->bindValue(4, $m->email);
+        $stmt->bindValue(5, $m->senha);
+        $stmt->bindValue(6, $m->data_cad);
+
+        $stmt->execute();
+        return $this->conexao->lastInsertId();
     }
 
-    public function delete()
+    public function delete(int $id) : bool
     {
+        $sql = "DELETE FROM correntista WHERE id=?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+        return $stmt->execute();
 
     }
 }
